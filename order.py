be2cdff36596487c76679e7fc1aac5d8a24a9d1c
UlrichVonRekkenin <ulrihlebedev@gmail.com>
2014-09-26 10:50:30 +0400

    Short print.

--- order.py
+++ order.py
@@ -10,19 +10,24 @@ from time import gmtime, strftime
 import collections
 
 def sort(filename):
-    with open(filename) as f:
+    d = dict()
 
-        d = dict()
-
+    with open(filename, 'r') as f:
         for line in [x.split(' ', 1) for x in f.readlines() if x != '\n' and not x.startswith('#')]:
             d[line[0]] = str.join(' ', sorted(line[1][:-1].split(' ')))
 
-        od = collections.OrderedDict(sorted(d.items()))
 
     with open(filename, 'w') as f:
-        f.write('## Last modified at {}\n## by {} script\n\n\n'.format(strftime("%d %m %Y, %H:%M:%S"), sys.argv[0]))
-        for k, v in od.items():
-            f.write('{} {}\n'.format(k, v))
+        od = collections.OrderedDict(sorted(d.items()))
+        print('## Last modified at {}\n## by {} script\n\n\n'.format(
+                    strftime("%d %m %Y, %H:%M:%S"),
+                    sys.argv[0]),
+                    str.join('\n',
+                            (
+                                str.format('{} {}', *kv) for i, kv in enumerate(od.items(), start = 1) if True
+                            )
+                        ),
+            file = f)
 
 if __name__ == '__main__':
     sort(sys.argv[1])
