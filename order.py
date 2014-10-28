37d551b4d121db37497602c654704c236f5bb156
ulrichvonrekkenin <ulrihlebedev@gmail.com>
2014-10-28 14:44:06 +0300

    FIXs.

--- order.py
+++ order.py
@@ -11,23 +11,24 @@ from shutil import copyfile
 
 
 def sort(filename):
-    d = dict()
 
-    with open(filename, 'r') as f:
+    d = dict()
+    copyfile(filename, '{}.bak'.format(filename))
+
+    with open(filename, 'r', encoding='utf-8') as f:
+
         for line in [x.split(' ', 1) for x in f.readlines() if x != '\n' and not x.startswith('#')]:
             uses = line[1][:-1].split(' ')
             uses.sort(key = lambda s: s.startswith('-'))
             d[line[0]] = str.join(' ', uses)
-            
-            copyfile(filename, '{}.bak'.format(filename))
 
 
-    with open(filename, 'w') as f:
-        f.write('## Last modified at {}\n\n{}'.format(
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('## Last modified at {time}\n\n{pkgs}'.format(
 
-            strftime("%d %m %Y, %H:%M:%S"),
+            time=strftime("%d.%m.%Y, %H:%M:%S"),
 
-            '\n'.join(
+            pkgs='\n'.join(
                 (
                     ('{} {}'.format(*kv)) for kv in sorted(d.items())
                 )
